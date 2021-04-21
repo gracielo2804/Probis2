873624064d51448268260b45d09c2828f4d94e32
Gracielo Justine <cielo.justine01@gmail.com>
2021-04-21 16:39:38 +0700

    LoginActivity

--- app/src/main/java/com/gracielo/probis2/LoginActivity.java
+++ app/src/main/java/com/gracielo/probis2/LoginActivity.java
@@ -3,17 +3,33 @@ package com.gracielo.probis2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.gracielo.probis2.databinding.ActivityMainBinding;
+import com.gracielo.probis2.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ActivityLoginBinding binding;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.etNoTelpLogin.getText().toString().trim().equalsIgnoreCase("")) {
+                    binding.etNoTelpLogin.setError("Field ini Harus diisi");
+                    binding.etNoTelpLogin.requestFocus();
+                }
+                if (binding.etPassLogin.getText().toString().isEmpty()) {
+                    binding.etPassLogin.setError("Field ini Harus diisi");
+                    binding.etPassLogin.requestFocus();
+                }
+            }
+        });
     }
 }
